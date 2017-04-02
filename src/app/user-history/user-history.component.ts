96d6b4bb922c6f1e1674d3fdcf38eedb19688cfe
RishabhKumar <rishabhkr1729@gmail.com>
2017-04-02 11:22:24 +0530

    Add appointments

--- src/app/user-history/user-history.component.ts
+++ src/app/user-history/user-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Appointment} from "../appointment";
+import {AppointmentService} from "../appointment.service";
+
 
 @Component({
   selector: 'app-user-history',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-history.component.css']
 })
 export class UserHistoryComponent implements OnInit {
+  status: number;
+  public appt: Appointment[]=[];
+  constructor(private appt_service: AppointmentService) {
+    this.appt_service.getAppointment().subscribe(
+      (data)=>{
+        console.log(data);
+        for(let item of data){
+        this.appt.push(new Appointment(item.patient_id,item.date,item.BeginTime,item.EndTime,item.mode,item.status,item.id));
 
-  constructor() { }
+        }
+console.log(this.appt);
+      });
+  }
 
   ngOnInit() {
   }
-
 }
